442b2fbaaa53cc0feead98cae8f4660cd39ad0a4
Liam Lei <775138281@qq.com>
2016-11-22 20:08:41 +0800

    updates

---
+++ innerclasses/BigEgg.java
@@ -0,0 +1,30 @@
+package innerclasses;
+
+/**
+ * Created by Liam on 22/11/2016.
+ */
+class Egg {
+    private Yolk y;
+    protected class Yolk {
+        public Yolk() {
+            System.out.println("Egg.Yolk()");
+        }
+    }
+
+    public Egg() {
+        System.out.println("New Egg()");
+        y = new Yolk();
+    }
+}
+
+public class BigEgg extends Egg {
+    public class Yolk {
+        public Yolk() {
+            System.out.println("BigEgg.Yolk()");
+        }
+    }
+
+    public static void main(String[] args) {
+        new BigEgg();
+    }
+}
