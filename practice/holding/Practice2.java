6f6ec2179cb626d0381ed9413ceac3f303d9ca25
Liam Lei <775138281@qq.com>
2016-11-24 09:00:55 +0800

    updates

---
+++ practice/holding/Practice2.java
@@ -0,0 +1,13 @@
+package practice.holding;
+
+import java.util.Set;
+import java.util.Collection;
+
+/**
+ * Created by Liam on 23/11/2016.
+ */
+public class Practice2 {
+    public static void main(String[] args) {
+        Collection<Integer> c = new Set<>();
+    }
+}
