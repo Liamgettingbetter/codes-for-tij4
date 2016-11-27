0384db7275cd1574326d8fa0c103f9105b640705
Liam Lei <775138281@qq.com>
2016-11-27 17:06:43 +0800

    updates

--- holding/SortedSetOfInteger.java
+++ holding/SortedSetOfInteger.java
@@ -1,16 +1,16 @@
 package holding;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.Random;
+import java.util.TreeSet;
+import java.util.SortedSet;
 
 /**
  * Created by Liam on 27/11/2016.
  */
-public class SetOfInteger {
+public class SortedSetOfInteger {
     public static void main(String[] args) {
         Random rand = new Random(47);
-        Set<Integer> intset = new HashSet<>();
+        SortedSet<Integer> intset = new TreeSet<>();
 
         for(int i = 0; i < 10000; i++)
             intset.add(rand.nextInt(30));
