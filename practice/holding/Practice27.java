b4147a3094eea60e67e4929de8f68904599c6577
Liam Lei <775138281@qq.com>
2016-11-28 21:12:45 +0800

    updates

--- practice/holding/Practice27.java
+++ practice/holding/Practice27.java
@@ -1,5 +1,6 @@
 package practice.holding;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -9,13 +10,17 @@ public class Practice27 {
     private class Command {
         private String id;
 
+        Command(String id) {
+            this.id = id;
+        }
+
         void operation() {
             System.out.println(id);
         }
     }
 
     private class Tools {
-        private Queue<Command> queue;
+        private Queue<Command> queue = new LinkedList<>();
 
         private Command inflate(Command obj) {
             queue.offer(obj);
@@ -25,9 +30,19 @@ public class Practice27 {
 
     private class Consume {
         void invoke(Queue<Command> q) {
-            for(Command c : q) {
-
-            }
+            while(!q.isEmpty())
+                q.poll().operation();
         }
+    }
+
+    public static void main(String[] args) {
+        Practice27 p27 = new Practice27();
+        Tools t = p27.new Tools();
+        Consume c = p27.new Consume();
+
+        for(int i = 0; i < 10; i++)
+            t.inflate(p27.new Command("Liam " + (i + 1)));
+        c.invoke(t.queue);
+
     }
 }
