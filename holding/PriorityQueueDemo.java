8c92a5c6c02ec92242e82e173a9b333fe9c74517
Liam Lei <775138281@qq.com>
2016-11-28 21:36:55 +0800

    updates

---
+++ holding/PriorityQueueDemo.java
@@ -0,0 +1,15 @@
+package holding;
+
+import java.util.Random;
+import java.util.PriorityQueue;
+
+/**
+ * Created by Liam on 28/11/2016.
+ */
+public class PriorityQueueDemo {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pQueue =
+                new PriorityQueue<>();
+        Random rand = new Random(47);
+    }
+}
