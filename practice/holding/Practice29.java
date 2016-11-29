6a6dde7c81733309ea18ab14ab45b0f5f23b4aad
Liam Lei <775138281@qq.com>
2016-11-29 22:08:44 +0800

    updates

---
+++ practice/holding/Practice29.java
@@ -0,0 +1,17 @@
+package practice.holding;
+
+import java.util.PriorityQueue;
+
+/**
+ * Created by Liam on 29/11/2016.
+ */
+class Simple {}
+
+public class Practice29 {
+    public static void main(String[] args) {
+        PriorityQueue<Simple> queue = new PriorityQueue<>();
+
+        for(int i = 0; i < 5; i++)
+            //queue.offer(new Simple());
+    }
+}
