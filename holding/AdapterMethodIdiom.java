152d397ccf8d57daf119781df2ef35564e20981a
Liam Lei <775138281@qq.com>
2016-11-30 22:09:58 +0800

    updates

---
+++ holding/AdapterMethodIdiom.java
@@ -0,0 +1,52 @@
+package holding;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by Liam on 30/11/2016.
+ */
+class ReversibleArrayList<T> extends ArrayList<T> {
+    public ReversibleArrayList(Collection<T> c) { super(c); }
+
+    public Iterable<T> reversed() {
+        return new Iterable<T>() {
+            public Iterator<T> iterator() {
+                return new Iterator<T>() {
+                    int current = size() - 1;
+
+                    @Override
+                    public boolean hasNext() {
+                        return current > -1;
+                    }
+
+                    @Override
+                    public T next() {
+                        return get(current--);
+                    }
+
+                    @Override
+                    public void remove() {
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
+        };
+    }
+}
+
+public class AdapterMethodIdiom {
+    public static void main(String[] args) {
+        ReversibleArrayList<String> ral =
+            new ReversibleArrayList<>(Arrays.asList("To be or not to be".split(" ")));
+
+        // Grabs the ordinary iterator via iterator() :
+        for(String s : ral)
+            System.out.print(s + " ");
+        System.out.println();
+        for(String s : ral.reversed())
+            System.out.print(s + " ");
+    }
+}
