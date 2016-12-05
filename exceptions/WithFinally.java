3bdd0317ac9e6f7cb18ff60d4e75a8a493c7874a
Liam Lei <775138281@qq.com>
2016-12-05 19:31:38 +0800

    updates

---
+++ exceptions/WithFinally.java
@@ -0,0 +1,25 @@
+package exceptions;
+
+/**
+ * Created by Liam on 05/12/2016.
+ */
+public class WithFinally {
+    static Switch sw = new Switch();
+
+    public static void main(String[] args) {
+        try {
+            sw.on();
+            // Code that can throw exceptions
+            OnOffSwitch.f();
+        }
+        catch(OnOffException1 e) {
+            System.out.println("OnOffException1");
+        }
+        catch(OnOffException2 e) {
+            System.out.println("OnOffException2");
+        }
+        finally {
+            sw.off();
+        }
+    }
+}
