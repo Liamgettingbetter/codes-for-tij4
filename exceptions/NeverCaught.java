f5c6e549b831edd4291e480a5aaf3495649454e2
Liam Lei <775138281@qq.com>
2016-12-05 14:52:07 +0800

    updates

---
+++ exceptions/NeverCaught.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/**
+ * Created by Liam on 05/12/2016.
+ */
+public class NeverCaught {
+    static void f() {
+        throw new RuntimeException("From f()");
+    }
+
+    static void g() {
+        f();
+    }
+
+    public static void main(String[] args) {
+        g();
+    }
+}
