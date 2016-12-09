21e92ec3e6f9e014c31c9bd653904a3fc9cf8e16
Liam Lei <775138281@qq.com>
2016-12-09 21:40:52 +0800

    updates

---
+++ practice/exceptions/Practice30.java
@@ -0,0 +1,13 @@
+package practice.exceptions;
+
+/**
+ * Created by Liam on 09/12/2016.
+ */
+class Annoyance extends RuntimeException {}
+class Sneeze extends Annoyance {}
+
+public class Practice30 {
+    public static void main(String[] args) {
+
+    }
+}
