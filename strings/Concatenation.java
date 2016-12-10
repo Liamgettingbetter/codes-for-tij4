35e3c5c681783bdb45fa5bfb5da817f9c20c882b
Liam Lei <775138281@qq.com>
2016-12-10 13:39:20 +0800

    update

---
+++ strings/Concatenation.java
@@ -0,0 +1,12 @@
+package strings;
+
+/**
+ * Created by Liam on 10/12/2016.
+ */
+public class Concatenation {
+    public static void main(String[] args) {
+        String mango = "mango";
+        String s = "abc" + mango + "def" + 47;
+        System.out.println(s);
+    }
+}
