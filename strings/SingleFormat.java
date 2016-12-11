0f07482cf6cfe42f674b3792eef29b1818d94b4d
Liam Lei <775138281@qq.com>
2016-12-11 10:48:23 +0800

    updates
    format output

---
+++ strings/SingleFormat.java
@@ -0,0 +1,17 @@
+package strings;
+
+/**
+ * Created by Liam on 11/12/2016.
+ */
+public class SingleFormat {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 5.332542;
+        // The old way
+        System.out.println("Row 1 : [" + x + " " + y + "]");
+        // The new way
+        System.out.format("Row 1 : [%d %f]\n", x, y);
+        // or
+        System.out.printf("Row 1 : [%d %f]\n", x, y);
+    }
+}
