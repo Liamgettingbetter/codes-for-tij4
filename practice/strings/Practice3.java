7da5cd561d050ace90edd9f5ff6a599e40a2fb3a
Liam Lei <775138281@qq.com>
2016-12-11 12:41:51 +0800

    updates
    updates about Formatter

---
+++ practice/strings/Practice3.java
@@ -0,0 +1,22 @@
+package practice.strings;
+
+import java.util.Formatter;
+
+/**
+ * Created by Liam on 11/12/2016.
+ */
+public class Practice3 {
+    private static class Turtle {
+        private String name;
+        private Formatter f;
+
+        Turtle(String name, Formatter f) {
+            this.name = name;
+            this.f    = f;
+        }
+
+        void move(int x, int y) {
+            f.format("%s This turtle is ")
+        }
+    }
+}
