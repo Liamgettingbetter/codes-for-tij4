12580ab36e4bde800933a9cb64294894841ccda2
Liam Lei <775138281@qq.com>
2016-12-12 15:32:09 +0800

    updates

---
+++ strings/IntegerMatch.java
@@ -0,0 +1,13 @@
+package strings;
+
+/**
+ * Created by Liam on 12/12/2016.
+ */
+public class IntegerMatch {
+    public static void main(String[] args) {
+        System.out.println("-1234".matches("-?\\d+"));
+        System.out.println("5678".matches("-?\\d+"));
+        System.out.println("+911".matches("-?\\d+"));
+        System.out.println("+911".matches("(-|\\+)?\\d+"));
+    }
+}
