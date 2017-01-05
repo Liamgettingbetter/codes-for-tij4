a5f3f8dac504e6eb6c1534b2f78e789267bb2ae5
Liam Lei <775138281@qq.com>
2017-01-05 21:51:44 +0800

    updates

---
+++ practice/typeinfo/Practice1.java
@@ -0,0 +1,27 @@
+package practice.typeinfo;
+
+/**
+ * Created by Liam on 05/01/2017.
+ */
+public class Practice1 {
+    private static class Toy {
+        Toy(int i) {};
+    }
+
+    public static void main(String[] args) {
+        try {
+            Class c = Class.forName("practice.typeinfo.Practice1.Toy");
+            Object obj = c.newInstance();
+        }
+        catch(ClassNotFoundException e) {
+            System.out.println("Can not find class.");
+            System.exit(1);
+        }
+        catch(InstantiationException e) {
+            e.printStackTrace();
+        }
+        catch(IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
