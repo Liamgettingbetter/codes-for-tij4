11dfed42b89eb907625771d0d467c58edb2bf420
Liam Lei <775138281@qq.com>
2017-01-09 22:23:55 +0800

    updates

--- practice/typeinfo/Practice8.java
+++ practice/typeinfo/Practice8.java
@@ -1,10 +1,27 @@
 package practice.typeinfo;
 
+
 /**
  * Created by Liam on 08/01/2017.
  */
 public class Practice8 {
-    static void print(Object obj) {
+    private static class A {}
+    private static class B extends A {}
+    private static class C extends B {}
+    private static class D extends C {}
+    private static class E extends D {}
 
+    static void print(Class obj) {
+        if(obj.getName().equals("java.lang.Object")) {
+            System.out.println(obj.getName());
+            return;
+        }
+
+        System.out.println(obj.getName());
+        print(obj.getSuperclass());
+    }
+
+    public static void main(String[] args) {
+        print(new E().getClass());
     }
 }
