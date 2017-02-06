383c16b3e88ae60310522b6a3cb3167ea2260b3c
Liam Lei <775138281@qq.com>
2017-02-06 22:43:30 +0800

    updates

---
+++ typeinfo/FamilyVsExactType.java
@@ -0,0 +1,27 @@
+package typeinfo;
+
+/**
+ * Created by Liam on 06/02/2017.
+ */
+class Base {}
+class Derived extends Base {}
+
+public class FamilyVsExactType {
+    static void test(Object x) {
+        System.out.println("Testing x of type " + x.getClass());
+        System.out.println("x instanceof Base " + (x instanceof Base));
+        System.out.println("x instanceof Derived " + (x instanceof Derived));
+        System.out.println("Base.isInstance(x) " + Base.class.isInstance(x));
+        System.out.println("Derived.isInstance(x) " + Derived.class.isInstance(x));
+        System.out.println("x.getClass(x) == Base.class " + (x.getClass() == Base.class));
+        System.out.println("x.getClass(x) == Derived.class " + (x.getClass() == Derived.class));
+        System.out.println("x.getClass(x).equals(Base.class) " + (x.getClass().equals(Base.class)));
+        System.out.println("x.getClass(x).equals(Derived.class) " + (x.getClass().equals(Derived.class)));
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        test(new Base());
+        test(new Derived());
+    }
+}
