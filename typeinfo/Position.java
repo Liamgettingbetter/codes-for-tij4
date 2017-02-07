b6aae4500dcd9f7b158a3b76c99f8d04883f625b
Liam Lei <775138281@qq.com>
2017-02-07 22:47:55 +0800

    updates

---
+++ typeinfo/Position.java
@@ -0,0 +1,45 @@
+package typeinfo;
+
+/**
+ * Created by Liam on 07/02/2017.
+ */
+public class Position {
+    private String title;
+    private Person person;
+
+    public Position(String title, Person employee) {
+        this.title = title;
+        this.person = employee;
+        if(person == null) person = Person.NULL;
+    }
+
+    public Position(String jobTitle) {
+        title = jobTitle;
+        this.person = Person.NULL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setTitle(String newTitle) {
+        title = newTitle;
+    }
+
+    public void setPerson(Person p) {
+        person = p;
+        if(person == null)
+            person = Person.NULL;
+    }
+
+    @Override
+    public String toString() {
+        return "Position : " + title + " " + person;
+    }
+
+
+}
