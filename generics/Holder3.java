c2a4cc09fa7e88ba09d25dc54fe682fdebcf029a
Liam Lei <775138281@qq.com>
2017-02-08 16:12:43 +0800

    updates

---
+++ generics/Holder3.java
@@ -0,0 +1,28 @@
+package generics;
+
+/**
+ * Created by Liam on 08/02/2017.
+ */
+public class Holder3<T> {
+    private T a;
+
+    public Holder3(T a) {
+        this.a = a;
+    }
+
+    public void set(T a) {
+        this.a = a;
+    }
+
+    public T get() {
+        return this.a;
+    }
+
+    public static void main(String[] args) {
+        Holder3<Automobile> h3 =
+                new Holder3<>(new Automobile());
+        Automobile a = h3.get();
+        // h3.set("Not an Automobile");
+        // h3.set(1);
+    }
+}
