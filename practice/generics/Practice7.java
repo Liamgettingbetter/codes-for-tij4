f335455d6e788c6f514813e78e930cba19b032a7
Liam Lei <775138281@qq.com>
2017-02-09 18:27:29 +0800

    updates

--- practice/generics/Practice7.java
+++ practice/generics/Practice7.java
@@ -1,12 +1,22 @@
 package practice.generics;
 
 import generics.Fibonacci;
+import java.util.Iterator;
 
 /**
  * Created by Liam on 09/02/2017.
  */
-public class Practice7 {
+public class Practice7 implements Iterable<Integer> {
     private Fibonacci f;
 
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return
+            }
+        }
+    }
 
 }
