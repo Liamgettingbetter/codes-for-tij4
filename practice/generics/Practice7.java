9150d76f28915a550979ae82d0a6c19dc5597860
Liam Lei <775138281@qq.com>
2017-02-11 17:07:24 +0800

    updates

--- practice/generics/Practice7.java
+++ practice/generics/Practice7.java
@@ -6,17 +6,38 @@ import java.util.Iterator;
 /**
  * Created by Liam on 09/02/2017.
  */
-/*
 public class Practice7 implements Iterable<Integer> {
+    private int recursive_times = 0;
     private Fibonacci f;
+
+    Practice7(int i, Fibonacci f) {
+        this.recursive_times = i;
+        this.f = f;
+    }
+
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
             @Override
             public boolean hasNext() {
-                return
+                return recursive_times > 0;
             }
-        }
+
+            @Override
+            public Integer next() {
+                recursive_times--;
+                return f.next();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        for(int i : new Practice7(18, new Fibonacci()))
+            System.out.print(i + " ");
     }
 }
-*/
