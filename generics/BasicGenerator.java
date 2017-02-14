c065ec7e63c1f3bce49d5758953b02d0219acf97
Liam Lei <775138281@qq.com>
2017-02-14 17:11:31 +0800

    updates

---
+++ generics/BasicGenerator.java
@@ -0,0 +1,28 @@
+package generics;
+
+import generics.coffee.Generator;
+
+/**
+ * Created by Liam on 14/02/2017.
+ */
+public class BasicGenerator<T> implements Generator<T> {
+    private Class<T> type;
+
+    public BasicGenerator(Class<T> type) {
+        this.type = type;
+    }
+
+    public T next() {
+        try {
+            return type.newInstance();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Produce a Default generator given a type token
+    public static <T> Generator<T> create(Class<T> type) {
+        return new BasicGenerator<T>(type);
+    }
+}
