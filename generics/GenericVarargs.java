28d0ca8541d8969b1fa5968f0e0ff9e784d317f6
Liam Lei <775138281@qq.com>
2017-02-14 11:50:49 +0800

    updates

--- generics/GenericVarargs.java
+++ generics/GenericVarargs.java
@@ -14,4 +14,13 @@ public class GenericVarargs {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        List<String> ls = makeList("A");
+        System.out.println(ls);
+        ls = makeList("A", "B", "C");
+        System.out.println(ls);
+        ls = makeList("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
+        System.out.println(ls);
+    }
 }
