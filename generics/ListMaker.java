77dd6dd8410f4a19c0d05b1f662f4f05f3a96de0
Liam Lei <775138281@qq.com>
2017-02-19 21:56:27 +0800

    updates

---
+++ generics/ListMaker.java
@@ -0,0 +1,17 @@
+package generics;
+
+import java.util.*;
+
+/**
+ * Created by Liam on 19/02/2017.
+ */
+public class ListMaker<T> {
+    List<T> create() {
+        return new ArrayList<T>();
+    }
+
+    public static void main(String[] args) {
+        ListMaker<String> stringMaker = new ListMaker<>();
+        List<String> stringList = stringMaker.create();
+    }
+}
