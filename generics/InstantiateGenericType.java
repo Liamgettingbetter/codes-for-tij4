7beab2e65e11e5517140d3f51c87cd3104e4df5c
Liam Lei <775138281@qq.com>
2017-02-20 18:12:09 +0800

    updates

---
+++ generics/InstantiateGenericType.java
@@ -0,0 +1,34 @@
+package generics;
+
+/**
+ * Created by Liam on 20/02/2017.
+ */
+class ClassAsFactory<T> {
+    T x;
+
+    public ClassAsFactory(Class<T> kind) {
+        try {
+            x = kind.newInstance();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Employee {}
+
+public class InstantiateGenericType {
+    public static void main(String[] args) {
+        ClassAsFactory<Employee> fe =
+                new ClassAsFactory<>(Employee.class);
+        System.out.println("ClassAsFactory<Employee> succeeded.");
+        try {
+            ClassAsFactory<Integer> fi =
+                    new ClassAsFactory<>(Integer.class);
+        }
+        catch(Exception e) {
+            System.out.println("ClassAsFactory<Integer> failed.");
+        }
+    }
+}
