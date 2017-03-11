15fd87f1fcfb7712f3febeaa1e1e85c0da98707f
Liam Lei <775138281@qq.com>
2017-03-11 23:01:50 +0800

    updates

--- generics/GenericWriting.java
+++ generics/GenericWriting.java
@@ -7,6 +7,25 @@ import java.util.*;
  */
 public class GenericWriting {
     static <T> void writeExact(List<T> list, T item) {
+        list.add(item);
+    }
+    static List<Apple> apples = new ArrayList<>();
+    static List<Fruit> f      = new ArrayList<>();
+    static void f1() {
+        writeExact(apples, new Apple());
+        writeExact(f, new Apple()); // ?
+    }
+    static <T> void writeWithWildcard(List<? super T > list, T item) {
+        list.add(item);
+    }
 
+    static void f2() {
+        writeWithWildcard(apples, new Apple());
+        writeWithWildcard(f, new Apple());
+    }
+
+    public static void main(String[] args) {
+        f1();
+        f2();
     }
 }
