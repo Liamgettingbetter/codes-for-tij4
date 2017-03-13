631131bbb04d995568bf90b62d40025f1d994b05
Liam Lei <775138281@qq.com>
2017-03-13 22:13:48 +0800

    updates

---
+++ generics/CaptureConversion.java
@@ -0,0 +1,27 @@
+package generics;
+
+/**
+ * Created by Liam on 13/03/2017.
+ */
+public class CaptureConversion {
+    static <T> void f1(Holder<T> holder) {
+        T t = holder.get();
+        System.out.println(t.getClass().getSimpleName());
+    }
+
+    static void f2(Holder<?> holder) {
+        f1(holder); // Call with captured type
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        Holder raw = new Holder<Integer>(1);
+        f1(raw);
+        f2(raw);
+        Holder rawBasic = new Holder();
+        rawBasic.set(new Object());
+        f2(rawBasic);
+        Holder<?> wildcarded = new Holder<Double>(1.0);
+        f2(wildcarded);
+    }
+}
