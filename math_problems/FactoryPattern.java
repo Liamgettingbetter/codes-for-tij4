2ff4e32e78a694e645e6ef0ef9b084a0e764cb59
Liam Lei <775138281@qq.com>
2017-03-29 20:16:39 +0800

    updates
    Java Design Pattern — Factory

---
+++ math_problems/FactoryPattern.java
@@ -0,0 +1,67 @@
+package math_problems;
+
+/**
+ * Created by Liam on 29/03/2017.
+ */
+interface Human {
+    void eat();
+    void sleep();
+    void play();
+}
+
+class Male implements Human {
+    @Override
+    public void eat() {
+        System.out.println("Man eat.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Man sleep.");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Man play.");
+    }
+}
+
+class Female implements Human {
+    @Override
+    public void eat() {
+        System.out.println("Female eat.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Female sleep.");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Female play.");
+    }
+}
+
+// Factory method
+class HumanFactory {
+    static Human create(String name) {
+        switch(name) {
+            case "Male":
+                return new Male();
+            case "Female" :
+                return new Female();
+            default:
+                return null;
+        }
+    }
+}
+
+public class FactoryPattern {
+    public static void main(String[] args) {
+        Human h = HumanFactory.create("Male");
+        h.eat();
+        h.sleep();
+        h.play();
+    }
+}
