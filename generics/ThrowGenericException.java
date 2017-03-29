57d59042505ca0165278a9671acb34ea60203522
Liam Lei <775138281@qq.com>
2017-03-29 14:08:17 +0800

    updates

--- generics/ThrowGenericException.java
+++ generics/ThrowGenericException.java
@@ -20,7 +20,7 @@ class ProcessRunner<T, E extends Exception> extends ArrayList<Processor<T,E>> {
 
 class Failure1 extends Exception {}
 
-class Process1 implements Processor<String, Failure1> {
+class Processor1 implements Processor<String, Failure1> {
     static int count = 3;
 
     public void process(List<String> resultCollector) throws Failure1 {
@@ -33,6 +33,46 @@ class Process1 implements Processor<String, Failure1> {
     }
 }
 
-public class ThrowGenericException {
+class Failure2 extends Exception {}
 
+class Processor2 implements Processor<Integer, Failure2> {
+    static int count = 2;
+
+    public void process(List<Integer> resultCollector) throws Failure2 {
+        if(count-- == 0)
+            resultCollector.add(47);
+        else
+            resultCollector.add(11);
+        if(count < 0)
+            throw new Failure2();
+    }
+}
+
+public class ThrowGenericException {
+    public static void main(String[] args) {
+        ProcessRunner<String, Failure1> runner =
+            new ProcessRunner<String, Failure1>();
+
+        for(int i = 0; i < 3; i++)
+            runner.add(new Processor1());
+
+        try {
+            System.out.println(runner.processAll());
+        }
+        catch(Failure1 e) {
+            System.out.println(e);
+        }
+
+        ProcessRunner<Integer, Failure2> runner2 =
+            new ProcessRunner<>();
+        for(int i = 0; i < 3; i++)
+            runner2.add(new Processor2());
+
+        try {
+            System.out.println(runner2.processAll());
+        }
+        catch(Failure2 e) {
+            System.out.println(e);
+        }
+    }
 }
