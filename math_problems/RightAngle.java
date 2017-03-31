10b756d1aff083259b81f6a8505f41d1b85bfd98
Liam Lei <775138281@qq.com>
2017-03-31 14:43:32 +0800

    updates

--- math_problems/RightAngle.java
+++ math_problems/RightAngle.java
@@ -13,14 +13,24 @@ public class RightAngle {
         int total_number = 0;
         int times = 0;
         int current_layer = 1;
+        int number_length = 0;
+        int temp;
 
         // make sure the total number n-layer will need.
         for(int i = 1; i <= layer; i++) {
             total_number += i;
         }
 
+        // make sure the max length among these numbers,
+        // in order to adjust the look of output triangle.
+        temp = total_number;
+        do {
+            number_length++;
+            temp /= 10;
+        } while(temp / 10 != 0);
+
         for(int i = 0; i < total_number; i++) {
-            System.out.printf("%02d ", (i + 1));
+            System.out.printf("%0*d ", number_length, (i + 1));
             times++;
 
             if(times == current_layer) {
