c31c7a3bc9c1a45b12ac6b53c4d3fe749abf7960
Liam Lei <775138281@qq.com>
2017-04-01 20:10:12 +0800

    updates

--- generics/decorator/Decoration.java
+++ generics/decorator/Decoration.java
@@ -60,8 +60,8 @@ public class Decoration {
     public static void main(String[] args) {
         TimeStamped t = new TimeStamped(new Basic());
         TimeStamped t2 = new TimeStamped(new SerialNumbered(new Basic()));
-
         // t2.getSerialNumber();
+        
         SerialNumbered s = new SerialNumbered(new Basic());
         SerialNumbered s2 = new SerialNumbered(new TimeStamped(new Basic()));
         // s2.getStamp();
