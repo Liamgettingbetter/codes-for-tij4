7abf9f3a72a58d5dc38b5a5e5900ec9d92fe97e2
Liam Lei <775138281@qq.com>
2017-04-03 20:40:16 +0800

    updates

--- arrays/ContainerComparison.java
+++ arrays/ContainerComparison.java
@@ -31,5 +31,14 @@ public class ContainerComparison {
             sphereList.add(new BerylliumSphere());
         System.out.println(sphereList);
         System.out.println(sphereList.get(4));
+
+        int[] integers = { 0, 1, 2, 3, 4, 5 };
+        System.out.println(Arrays.toString(integers));
+        System.out.println(integers[4]);
+
+        List<Integer> intList = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5));
+        intList.add(97);
+        System.out.println(intList);
+        System.out.println(intList.get(4));
     }
 }
