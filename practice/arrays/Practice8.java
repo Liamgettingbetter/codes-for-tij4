d04c885eac4d4783bff6c56d7c70a694e592108c
Liam Lei <775138281@qq.com>
2017-04-04 18:05:36 +0800

    updates

---
+++ practice/arrays/Practice8.java
@@ -0,0 +1,15 @@
+package practice.arrays;
+
+/**
+ * Created by Liam on 04/04/2017.
+ */
+public class Practice8 {
+    private static Object[] a;
+
+    public static void main(String[] args) {
+        a = new Object[5];
+        a[0] = new String("Fuck");
+        a[1] = new Integer(1);
+        a[2] = new Double(6.66);
+    }
+}
