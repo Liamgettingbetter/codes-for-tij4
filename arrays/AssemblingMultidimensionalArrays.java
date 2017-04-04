0b183ce897a4a1cca983ea3493b7ecf63990e691
Liam Lei <775138281@qq.com>
2017-04-04 15:33:25 +0800

    updates

---
+++ arrays/AssemblingMultidimensionalArrays.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by Liam on 04/04/2017.
+ */
+public class AssemblingMultidimensionalArrays {
+    public static void main(String[] args) {
+        Integer[][] a;
+
+        a = new Integer[3][];
+        for(int i = 0; i < a.length; i++) {
+            a[i] = new Integer[3];
+            for(int j = 0; j < a[i].length; j++) {
+                a[i][j] = i * j;
+            }
+        }
+        System.out.println(Arrays.deepToString(a));
+    }
+
+}
