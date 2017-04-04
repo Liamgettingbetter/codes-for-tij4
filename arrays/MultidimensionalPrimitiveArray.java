2d0672c235f7227bc57bd27154cbadb111dcc409
Liam Lei <775138281@qq.com>
2017-04-04 15:09:02 +0800

    updates

---
+++ arrays/MultidimensionalPrimitiveArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by Liam on 04/04/2017.
+ */
+public class MultidimensionalPrimitiveArray {
+    public static void main(String[] args) {
+        int[][] a = {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+        };
+
+        System.out.println(Arrays.deepToString(a));
+    }
+}
+
