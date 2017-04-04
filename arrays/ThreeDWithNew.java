f665136785634dc484d28a6f42232ab3ec5a12cf
Liam Lei <775138281@qq.com>
2017-04-04 15:18:37 +0800

    updates

---
+++ arrays/ThreeDWithNew.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by Liam on 04/04/2017.
+ */
+public class ThreeDWithNew {
+    public static void main(String[] args) {
+        // 3-D array with fixed length
+        int [][][] a = new int[2][2][4];
+        System.out.println(Arrays.deepToString(a));
+    }
+}
