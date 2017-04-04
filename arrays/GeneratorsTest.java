d55af585b3d35f8f281b19395e33608faf4dda7b
Liam Lei <775138281@qq.com>
2017-04-04 20:04:46 +0800

    updates

---
+++ arrays/GeneratorsTest.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import generics.coffee.Generator;
+
+/**
+ * Created by Liam on 04/04/2017.
+ */
+public class GeneratorsTest {
+    public static int size = 10;
+
+    public static void test(Class<?> surroundingClass) {
+        for(Class<?> type : surroundingClass.getClasses()) {
+            System.out.println(type.getSimpleName() + ": ");
+
+            try {
+                Generator<?> g = (Generator<?>)type.newInstance();
+                for(int i = 0; i < size; i++)
+                    System.out.printf(g.next() + " ");
+                System.out.println();
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        test(CountingGenerator.class);
+    }
+}
