e44bd4da403d24da16d6e2dc5f3a5a576fcdc5ab
Liam Lei <775138281@qq.com>
2017-04-04 16:34:30 +0800

    updates

--- arrays/ParameterizedArrayType.java
+++ arrays/ParameterizedArrayType.java
@@ -19,10 +19,8 @@ public class ParameterizedArrayType {
     public static void main(String[] args) {
         Integer[] ints = { 1, 2, 3, 4, 5 };
         Double[] doubles = { 1.1, 2.2, 3.3, 4.4, 5.5 };
-        Integer[] ints2 =
-            new ClassParameter<Integer>().f(ints);
-        Double[] double2 =
-            new ClassParameter<Double>().f(doubles);
+        Integer[] ints2 = new ClassParameter<Integer>().f(ints);
+        Double[] double2 = new ClassParameter<Double>().f(doubles);
         ints2 = MethodParameter.f(ints);
         double2 = MethodParameter.f(doubles);
     }
