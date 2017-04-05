bb5a64e9b2b082c37a59522eccdc7a8ea34b4770
Liam Lei <775138281@qq.com>
2017-04-05 11:10:38 +0800

    updates

---
+++ arrays/RandomGeneratorTest.java
@@ -0,0 +1,10 @@
+package arrays;
+
+/**
+ * Created by Liam on 05/04/2017.
+ */
+public class RandomGeneratorTest {
+    public static void main(String[] args) {
+        GeneratorsTest.test(RandomGenerator.class);
+    }
+}
