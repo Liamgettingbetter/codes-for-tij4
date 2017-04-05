e418390e006d4dd5be9fcbfa9679d81493981214
Liam Lei <775138281@qq.com>
2017-04-05 16:17:28 +0800

    updates

---
+++ practice/arrays/Practice20.java
@@ -0,0 +1,15 @@
+package practice.arrays;
+
+import java.util.Arrays;
+
+/**
+ * Created by Liam on 05/04/2017.
+ */
+public class Practice20 {
+    public static void main(String[] args) {
+        int[][] show = new int[3][3];
+        int[][] fuck = new int[3][3];
+
+        System.out.println(Arrays.deepEquals(show, fuck));
+    }
+}
