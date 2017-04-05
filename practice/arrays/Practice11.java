449583250e3c26448c662d071db2d08c40d6730f
Liam Lei <775138281@qq.com>
2017-04-05 11:36:34 +0800

    updates

---
+++ practice/arrays/Practice11.java
@@ -0,0 +1,10 @@
+package practice.arrays;
+
+/**
+ * Created by Liam on 05/04/2017.
+ */
+public class Practice11 {
+    public static void main(String[] args) {
+        // Integer[] a = new int[10];
+    }
+}
