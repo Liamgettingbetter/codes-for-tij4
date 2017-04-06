375e6fab464ca0e702a9ac5dd66fb9f2e7480a67
Liam Lei <775138281@qq.com>
2017-04-06 17:41:28 +0800

    updates

--- algorithms/SelectionSort.java
+++ algorithms/SelectionSort.java
@@ -7,7 +7,8 @@ import java.util.Comparator;
  */
 public class SelectionSort {
     // This class should not be initialized.
-    private SelectionSort() {}
+    private SelectionSort() {
+    }
 
     // Check if the first elements is less than the second one.
     private static boolean less(Comparable x, Comparable y) {
@@ -26,39 +27,67 @@ public class SelectionSort {
     }
 
     /**
-     *  Reorder this array in ascending way, using the natural order.
-     *  @param a the array to be ordered.
+     * Reorder this array in ascending way, using the natural order.
+     *
+     * @param a the array to be ordered.
      */
     public static void sort(Comparable[] a) {
         int length = a.length;
 
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             int min = i;
 
             // Find the smallest element's index, then assign it to min.
-            for(int j = i + 1; j < length; j++) {
-                if(less(a[j], min)) min = j;
+            for (int j = i + 1; j < length; j++) {
+                if (less(a[j], min)) min = j;
             }
             exchange(a, i, min);
+            assert isSorted(a, 0, i);
         }
+        assert isSorted(a);
     }
 
     /**
-     *  Reorder this array in ascending way, using a comparator.
-     *  @param a the array
-     *  @param comparator the comparator specifying the order.
+     * Reorder this array in ascending way, using a comparator.
+     *
+     * @param a          the array
+     * @param comparator the comparator specifying the order.
      */
     public static void sort(Object[] a, Comparator comparator) {
         int length = a.length;
 
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             int min = i;
 
             // Find the smallest element's index, then assign it to him.
-            for(int j = i + 1; j < length; j++) {
-                if(less(comparator, j, min)) min = j;
+            for (int j = i + 1; j < length; j++) {
+                if (less(comparator, j, min)) min = j;
             }
             exchange(a, i, min);
+            assert isSorted(a, comparator, 0, i);
         }
+        assert isSorted(a, comparator);
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int low, int high) {
+        for(int i = low + 1; i <= high; i++) {
+            if(less(i, i - 1)) return false;
+        }
+        return true;
+    }
+
+    private static boolean isSorted(Object[] a, Comparator comp) {
+        return isSorted(a, comp, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(Object[] a, Comparator comp, int low, int high) {
+        for(int i = low + 1; i <= high; i++) {
+            if(less(comp, i, i - 1)) return false;
+        }
+        return true;
     }
 }
