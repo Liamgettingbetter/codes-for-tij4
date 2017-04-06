c329d00768ee8178a8459996aea032f66ee44729
Liam Lei <775138281@qq.com>
2017-04-06 12:49:01 +0800

    updates

--- algorithms/Date.java
+++ algorithms/Date.java
@@ -3,7 +3,7 @@ package algorithms;
 /**
  * Created by Liam on 06/04/2017.
  */
-public class Date {
+public class Date implements Comparable<Date> {
     private static final int[] DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
     private final int year;     // such as 2012
@@ -18,6 +18,7 @@ public class Date {
      *  @throws IllegalArgumentException if this date is invalid.
      */
     public Date(int year, int month, int day) {
+        if(!isValid(year, month, day)) throw new IllegalArgumentException("Invalid date");
         this.year = year;
         this.month = month;
         this.day = day;
@@ -30,10 +31,11 @@ public class Date {
      */
     public Date(String date) {
         String[] temp = date.split("/");
-
-        int month = Integer.parseInt(temp[0]);
-        int day   = Integer.parseInt(temp[1]);
-        int year  = Integer.parseInt(temp[2]);
+        if(temp.length != 3) throw new IllegalArgumentException("Invalid date");
+        this.month = Integer.parseInt(temp[0]);
+        this.day   = Integer.parseInt(temp[1]);
+        this.year  = Integer.parseInt(temp[2]);
+        if(!isValid(year, month, day)) throw new IllegalArgumentException("Invalid date");
     }
 
     /**
@@ -71,5 +73,89 @@ public class Date {
     // is it a leap year?
     private static boolean isLeapYear(int year) {
         return year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
+    }
+
+    /**
+     *  Returns the next date in the calendar.
+     *  @return a date that represents the next day after this day.
+     */
+    public Date next() {
+        if(isValid(year, month, day + 1)) return new Date(year, month ,day + 1);
+        else if(isValid(year, month + 1, 1)) return new Date(year, month + 1, 1);
+        else return new Date(year + 1, 1, 1);
+    }
+
+    /**
+     *  Compares two dates chronologically.
+     *  @param other the other date.
+     *  @return true if other precedes this day, false otherwise.
+     */
+    public boolean isBefore(Date other) {
+        return compareTo(other) < 0;
+    }
+
+    /**
+     *  Compares two dates chrnologically.
+     *  @param other the other date.
+     *  @return true if other date is later than this date.
+     */
+    public boolean isAfter(Date other) {
+        return compareTo(other) > 0;
+    }
+
+    /**
+     *  Compares two dates chronologically.
+     *  @return the value 0 if the argument is equals to this date.
+     *          a negative integer if the argument is after this date.
+     *          a positive integer if the argument is before this date.
+     */
+    @Override
+    public int compareTo(Date other) {
+      if(this.year > other.getYear()) return 1;
+      if(this.year < other.getYear()) return -1;
+      if(this.month > other.getMonth()) return 1;
+      if(this.month < other.getMonth()) return -1;
+      if(this.day > other.getDay()) return 1;
+      if(this.day < other.getDay()) return -1;
+      return 0;
+    }
+
+    /**
+     *  Return the string format of this date.
+     *  @return a string representing this date.
+     */
+    @Override
+    public String toString() {
+        return month + "/" + day + "/" + year;
+    }
+
+    /**
+     *  Returns an Integer hash code for this date.
+     *  @return an integer hash code for this date.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 17;
+        hash = 31 * hash + month;
+        hash = 31 * hash + day;
+        hash = 31 * hash + year;
+        return hash;
+    }
+
+    /**
+     *  Unit tests for the Date data type.
+     *  @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        Date today = new Date(2017, 4, 6);
+        System.out.println(today);
+
+        for(int i = 0; i < 10; i++) {
+            today = today.next();
+            System.out.println();
+        }
+        System.out.println(today.isAfter(today.next()));
+        System.out.println(today.isBefore(today));
+        System.out.println(today.next().isAfter(today));
     }
 }
