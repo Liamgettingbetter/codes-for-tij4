fe7655b1db09ddd1755e33eb79cdbd815dab97b6
Liam Lei <775138281@qq.com>
2017-04-10 21:46:39 +0800

    updates

--- io/DirList1.java
+++ io/DirList1.java
@@ -21,7 +21,7 @@ class DirFilter implements FilenameFilter {
     }
 }
 
-public class DirList3 {
+public class DirList1 {
     public static void main(final String[] args) {
         File path = new File(".");
         String[] list;
@@ -29,15 +29,7 @@ public class DirList3 {
         if(args.length == 0)
             list = path.list();
         else
-            list = path.list(new FilenameFilter() {
-                private Pattern p = Pattern.compile(args[0]);
-
-                @Override
-                public boolean accept(File dir, String name) {
-                    return p.matcher(name).matches();
-                }
-            });
-
+            list = path.list(new DirFilter(args[0]));
         Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
         for(String item : list)
             System.out.println(item);
