c6ff7cb9038b4e762312627a01713e70e27a8d28
Liam Lei <775138281@qq.com>
2017-04-10 21:44:05 +0800

    update

--- io/DirList3.java
+++ io/DirList3.java
@@ -8,6 +8,19 @@ import java.util.regex.Pattern;
 /**
  * Created by Liam on 10/04/2017.
  */
+class DirFilter implements FilenameFilter {
+    private Pattern p;
+
+    public DirFilter(String regex) {
+        p = Pattern.compile(regex);
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return p.matcher(name).matches();
+    }
+}
+
 public class DirList3 {
     public static void main(final String[] args) {
         File path = new File(".");
