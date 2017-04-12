fb1374b12185ec2becbdd485f617ce0caa67a3ca
Liam Lei <775138281@qq.com>
2017-04-12 18:38:05 +0800

    update

---
+++ io/DirList3.java
@@ -0,0 +1,29 @@
+package io;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import java.util.regex.*;
+
+/**
+ * Created by Liam on 12/04/2017.
+ */
+public class DirList3 {
+    public static void main(String[] args) {
+        File path = new File(".");
+        String[] list;
+
+        if(args.length == 0)
+            list = path.list();
+        else
+            list = path.list(new FilenameFilter() {
+                private Pattern p = Pattern.compile(args[0]);
+
+                @Override
+                public boolean accept(File dir, String filename) {
+                    return p.matcher(filename).matches();
+                }
+            });
+        
+    }
+}
